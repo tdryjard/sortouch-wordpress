75eb74990f38ea249f605a33449a3d7d1341fc5b
Thomas.dryjard <newwbuiss@gmail.com>
2020-05-22 14:20:36 +0200

    plugin v0.1

---
+++ sortouch-plugin/sortouch/build/precache-manifest.2f12ac42358bdda77250c10eeb89e86f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748bb14845f43922287714b14d68479d",
+    "url": "/wp-content/plugins/sortouch/sortouch/build/index.html"
+  },
+  {
+    "revision": "a8ea3e97c79f9a23237f",
+    "url": "/wp-content/plugins/sortouch/sortouch/build/static/css/main.4e22dc30.chunk.css"
+  },
+  {
+    "revision": "ce05dec8ce20f8a3a869",
+    "url": "/wp-content/plugins/sortouch/sortouch/build/static/js/2.3c00815d.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/wp-content/plugins/sortouch/sortouch/build/static/js/2.3c00815d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8ea3e97c79f9a23237f",
+    "url": "/wp-content/plugins/sortouch/sortouch/build/static/js/main.7bd7f15c.chunk.js"
+  },
+  {
+    "revision": "05da5ee124ff3cfe6c69",
+    "url": "/wp-content/plugins/sortouch/sortouch/build/static/js/runtime-main.00b83387.js"
+  },
+  {
+    "revision": "c92d05ebbdb4c63d69dc1c119a69e0ee",
+    "url": "/wp-content/plugins/sortouch/sortouch/build/static/media/Sen-Regular.c92d05eb.ttf"
+  }
+]);
